92fc6a84e2c779260fd8b00a91df21c5729824a8
VietDM <saoxa37@gmail.com>
2022-06-05 05:02:27 +0700

    Change title

--- resources/views/auth/base.blade.php
+++ resources/views/auth/base.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>Solopayment</title>
+    <title>AI DIGITAL MEDIA</title>
     <link rel="shortcut icon" href="{{ asset('image/home-page/icon-mini.png') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
