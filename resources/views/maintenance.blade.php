f50ced556e163a55bc4b86c7b772c7302e81b167
VietDM <saoxa37@gmail.com>
2022-06-04 23:40:10 +0700

    view maintenance

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AI DIGITAL MEDIA</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/amd.css') }}">
+</head>
+<body>
+<div class="wrapper maintenance ready">
+    <div class="main-content" id="main-content">
+        <div class="img-main">
+            <img src="{{ asset('image/main_maintenance.png') }}" alt="Main">
+            <img src="{{ asset('image/f_main_maintenance.png') }}" alt="Main" class="mt-3">
+        </div>
+    </div>
+</div>
+
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/popper.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
