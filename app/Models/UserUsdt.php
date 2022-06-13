3d049226a5bf9a851a92ebb604a1264ffb8cf2b5
VietDM <saoxa37@gmail.com>
2022-06-13 15:11:48 +0700

    a

--- app/Models/UserUsdt.php
+++ app/Models/UserUsdt.php
@@ -20,22 +20,11 @@ class UserUsdt extends Model
 
     public static function createUsdtAccount($user_id): bool
     {
-        $path = app_path('Python/create_account.py');
-        exec("python3 $path $user_id", $output);
-        if ($output[0] == 'Fail') {
-            return false;
-        }
         try {
-            $data = file_get_contents(app("Python/data/create_account_$user_id.txt"));
-            $data = explode(' ||| ', $data);
-            unlink(app("Python/data/create_account_$user_id.txt"));
-
-            $model = new self;
-            $model->user_id = $user_id;
-            $model->token = $data[0];
-            $model->private_key = $data[1];
-            $model->save();
-            return true;
+            $path = app_path('Python/create_account.py');
+            $output = shell_exec("python3 $path $user_id");
+            dd($output);
+            return $output[0] == 'Done';
         } catch (Exception $exception) {
             return false;
         }
