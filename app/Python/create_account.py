5d2cf24096bac15393b9a509e7e57d0ae43e088c
VietDM <saoxa37@gmail.com>
2022-06-13 14:58:48 +0700

    a

--- app/Python/create_account.py
+++ app/Python/create_account.py
@@ -1,6 +1,6 @@
 import json
+import mysql.connector
 import sys
-import os
 from web3 import Web3
 
 url = 'https://mainnet.infura.io/v3/2b0d5ad9f5854ca5b8d168eb72477e91'
@@ -8,14 +8,23 @@ url = 'https://mainnet.infura.io/v3/2b0d5ad9f5854ca5b8d168eb72477e91'
 try:
     user_id = sys.argv[1]
 
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="cdmllove",
+        database="adm_db"
+    )
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO user_usdt (user_id, token, private_key) VALUES (%d, %s, %s)"
+
     web3 = Web3(Web3.HTTPProvider(url))
     account = web3.eth.account.create()
     address = account.address
-    privateKey = account.privateKey
+    privateKey = account.privateKey.hex()
 
-    f = open(os.getcwd() + '/create_account_' + user_id + '.txt', 'a')
-    f.write(address + ' ||| ' + privateKey.hex())
-    f.close()
+    val = (int(user_id), address, privateKey)
+    mycursor.execute(sql, val)
+    mydb.commit()
 
     print('Done')
 except:
