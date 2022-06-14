3dbe495db01a31b2df172749e08bcdcbfe4d17d2
VietDM <saoxa37@gmail.com>
2022-06-14 10:22:50 +0700

    Abc

--- config/botman/telegram.php
+++ config/botman/telegram.php
@@ -12,15 +12,4 @@ return [
     |
     */
     'token' => env('TELEGRAM_BOT_TOKEN'),
-
-    'bots' => [
-        'mybot' => [
-            'username'            => env('TELEGRAM_BOT', 'TelegramBot'),
-            'token'               => env('TELEGRAM_BOT_TOKEN', ''),
-            'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', ''),
-            'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', ''),
-            'commands'            => [
-            ],
-        ],
-    ],
 ];
